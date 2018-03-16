80664f77fd211b4bf8b6073012a5582e47863d2b
Rosario Yoshiharu Yoshida <rosario.yoshida@gmail.com>
2018-03-15 22:35:21 -0300

    delete

--- target/trackmycity-integration/App/model/AlertType.js
+++
@@ -1,8 +0,0 @@
-Ext.define('App.model.AlertType', {
-	extend: 'Ext.data.Model',
-	fields: [
-	         {name: 'id', type: 'long'},
-	         {name: 'name', type: 'string'},
-	         {name: 'iconMap', type: 'string'}
-	        ]
-});
--- target/trackmycity-integration/App/model/Capitulo.js
+++
@@ -1,9 +0,0 @@
-Ext.define.('App.model.Capitulo', {
-		extend: 'Ext.data.Model',
-		fields: [
-	         {name: 'id', type: 'long'},
-	         {name: 'titulo', type: 'string'},
-	         {name: 'numero:', type: 'int'}
-        ]
-	}
-);
